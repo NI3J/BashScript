25f222fee2818e0cf3772f30df20508c4e353627
NI3J <jadhavnitin75@gmail.com>
2021-11-13 13:47:56 +0530

    shells files

---
+++ MultiComment.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+<<COMMENTS
+This is the first comment
+This is the second comment
+This is the third comment
+COMMENTS
+
+echo "Hello World"
---
+++ comment.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This is a single-line comment in bash script
+echo "Enter your name:"
+read name
+echo
+#echo output,its also a single line comment
+echo "The current user name is $name"
+#This is another single line comment
